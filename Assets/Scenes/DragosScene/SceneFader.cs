f75a9c83ca73828bf7db0a3cccac89b19a4e94a3
Dragos <dragos.boboluta@yahoo.com>
2026-01-31 17:52:35 +0200

    add menu

---
+++ Assets/Scenes/DragosScene/SceneFader.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneFader : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+
+    void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeOut(sceneName));
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = fadeDuration;
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            fadeImage.color = new Color(0, 0, 0, t / fadeDuration);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut(string sceneName)
+    {
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            fadeImage.color = new Color(0, 0, 0, t / fadeDuration);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
